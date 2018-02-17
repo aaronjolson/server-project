1db2071bb6af85a5157fca2ee802cd6006f8e8c0
Aaron Olson <aaolson817@gmail.com>
2018-02-17 14:34:47 -0600

    Enhancements to the fileserver

--- src/com/company/UDPImageServer.java
+++ src/com/company/UDPImageServer.java
@@ -1,9 +1,11 @@
 package com.company;
 
-import java.io.*;
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
-import java.util.Arrays;
+import java.net.SocketTimeoutException;
 
 public class UDPImageServer {
 
@@ -11,7 +13,6 @@ public class UDPImageServer {
         DatagramSocket serverSocket = new DatagramSocket(4000);
         int packetsize = 1024;
         double allBytes = 0;
-        double numberOfPackets = Math.ceil(((new File("C:/Users/aaols/IdeaProjects/timeserver/src/com/company/eth.jpg")).length()) / packetsize);
         FileOutputStream fileOutputStream = new FileOutputStream("output.jpg");
 
         BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(fileOutputStream);
@@ -19,16 +20,24 @@ public class UDPImageServer {
         byte[] mybytearray = new byte[packetsize];
         DatagramPacket receivePacket = new DatagramPacket(mybytearray, mybytearray.length);
 
-        System.out.println(numberOfPackets);
-
-        for (int i = 0; i < numberOfPackets + 3; i++){
-            serverSocket.receive(receivePacket);
-            byte binaryData[] = receivePacket.getData();
-            allBytes += binaryData.length;
-            System.out.println("Packet: " + (i + 1) +
-                    " - " + String.format("%d",(long)(allBytes - packetsize)) +
-                    " - " + String.format("%d",(long)allBytes));
-            bufferedOutputStream.write(binaryData, 0, binaryData.length);
+        System.out.println("Starting Server...");
+        boolean x = true;
+        int i = 0;
+        while (x) {
+            try {
+                serverSocket.receive(receivePacket);
+                serverSocket.setSoTimeout(5000);
+                byte binaryData[] = receivePacket.getData();
+                allBytes += binaryData.length;
+                System.out.println("Packet: " + (i + 1) +
+                        " - " + String.format("%d", (long) (allBytes - packetsize)) +
+                        " - " + String.format("%d", (long) allBytes));
+                i += 1;
+                bufferedOutputStream.write(binaryData, 0, binaryData.length);
+            } catch (SocketTimeoutException s) {
+                    System.out.println("Transfer Finished");
+                    break;
+            }
         }
     }
 }
